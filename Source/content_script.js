657064ba6723dc8e176b769d83a3ca8d21424e7d
dhamster <hamilton.david.r@gmail.com>
2016-06-09 00:39:36 -0500

    Add files via upload

---
+++ Source/content_script.js
@@ -0,0 +1,69 @@
+walk(document.body);
+
+if (window.MutationObserver) {
+	var observer = new MutationObserver(function (mutations) {
+		Array.prototype.forEach.call(mutations, function (m) {
+			if (m.type === 'childList') {
+				walk(m.target);
+			} else if (m.target.nodeType === 3) {
+				handleText(m.target);
+			}
+		});
+	});
+
+	observer.observe(document.body, {
+		childList: true,
+		attributes: false,
+		characterData: true,
+		subtree: true
+	});
+}
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// https://github.com/alexhong/sjw-to-skeleton
+	// They, in turn, stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	if (textNode.parentElement.tagName.toLowerCase() === "script" || textNode.parentElement.isContentEditable === true) {
+		return false;
+	}
+
+	var oldValue = textNode.nodeValue;
+	var v = oldValue;
+
+	v = v.replace(/\bcuck(s?)\b/ig, "scary ghost$1");
+	v = v.replace(/\bcucked\b/ig, "spooked");
+	v = v.replace(/\bcucking\b/ig, "spooking");
+	v = v.replace(/\bcuckservative\b/ig, "spookmaster");
+	v = v.replace(/\bnsfcucks\b/ig, "dumb");
+	
+	if (v !== oldValue) {
+		textNode.nodeValue = v;
+	}
+}
